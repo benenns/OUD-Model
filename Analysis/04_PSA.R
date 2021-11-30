5ad8a9a3bea5650960c2308d66e8c843caed96e3
Benjamin Enns <ben.enns@gmail.com>
2021-11-29 16:08:11 -0800

    Test PSA results

--- Analysis/04_PSA.R
+++ Analysis/04_PSA.R
@@ -69,7 +69,7 @@ df_psa_params <- generate_psa_params(n_sim = 1000, seed = 3730687, n_samp = 250,
 
 ### Run decision model on each parameter set of PSA input dataset to produce
 ### PSA outputs for cost and effects
-n_sim <- 20 # just to test function
+n_sim <- 250 # just to test function
 
 # Initialize data frames
 df_outcomes_MET_PSA <- data.frame()
